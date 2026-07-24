ef85e3c1ae29c59470e744db587329de0d4963ea
Faraji Ombonya <farajiombonya@gmail.com>
2026-07-24 09:35:57 +0300

    Add the notification model and its migrations

--- django_messenger/models.py
+++ django_messenger/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class NotificationManager(models.Manager):
+    def mark_as_read(self):
+        return self.update(is_read=True)
+
+    def unread(self):
+        return self.filter(is_read=False)
+
+    def read(self):
+        return self.filter(is_read=False)
+
+
+class Notification(models.Model):
+    data = models.JSONField()
+    is_read = models.BooleanField(default=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    read_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = NotificationManager
